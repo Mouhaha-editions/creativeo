6daafc38c6430f7a006da991fc46204ce17726a0
Pierrick <p.pobelle@id2i.net>
2020-01-24 13:29:49 +0100

    + dashboard

---
+++ src/Service/InventoryService.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace App\Service;
+
+
+use App\Entity\Inventory;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\VarDumper\VarDumper;
+
+class InventoryService
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function countTotalPrice(UserInterface $user)
+    {
+        $result = $this->entityManager->getRepository(Inventory::class)
+            ->createQueryBuilder('i')
+            ->select('SUM(i.quantity*i.price)')
+            ->where('i.user = :user')
+            ->setParameter('user', $user)
+            ->groupBy('i.user')
+            ->getQuery()->getOneOrNullResult();
+
+        return array_pop($result);
+    }
+    public function countQuantity(UserInterface $user)
+    {
+        $result = $this->entityManager->getRepository(Inventory::class)
+            ->createQueryBuilder('i')
+            ->select('COUNT(i)')
+            ->where('i.user = :user')
+            ->setParameter('user', $user)
+            ->groupBy('i.user')
+            ->getQuery()->getOneOrNullResult();
+        return array_pop($result);
+    }
+}
