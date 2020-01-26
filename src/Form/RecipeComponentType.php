f1ad5bae3b802de12ebc143457211318ffef940e
pkshetlie <pierrick.pobelle@gmail.com>
2020-01-26 14:50:15 +0100

    Privatisation des recettes

--- src/Form/RecipeComponentType.php
+++ src/Form/RecipeComponentType.php
@@ -9,30 +9,37 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class RecipeComponentType extends AbstractType
 {
 
-    public function __construct()
-    {
+    /**
+     * @var \Symfony\Component\Security\Core\Authentication\Token\TokenInterface|null
+     */
+    private $token;
 
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->token = $tokenStorage->getToken();
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $user = $this->token->getUser();
         $builder
             ->add('component', EntityType::class, [
                 "choice_label" => "label",
-                'label'=>'entity.recipe_component.component',
+                'label' => 'entity.recipe_component.component',
 
                 "class" => Component::class,
-                "query_builder" => function (EntityRepository $er) {
-                    return $er->createQueryBuilder('c')->orderBy('c.label');
+                "query_builder" => function (EntityRepository $er) use($user) {
+                    return $er->createQueryBuilder('c')->where('c.user = :user')->setParameter('user', $user)->orderBy('c.label');
                 },
-        ])
-        ->add('quantity',null,[
-            'label'=>'entity.recipe_component.quantity'
-        ]);
+            ])
+            ->add('quantity', null, [
+                'label' => 'entity.recipe_component.quantity'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
