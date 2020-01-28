6177942ae1b7a442413266b5a7944f234f1d2891
Pierrick <p.pobelle@id2i.net>
2020-01-28 13:16:52 +0100

    debut des taxes

--- src/Entity/Taxe.php
+++ src/Entity/Taxe.php
@@ -5,12 +5,25 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\TaxeRepository")
  */
 class Taxe
 {
+    public const TYPE_PERCENTAGE = 10;
+    public const TYPE_AMOUNT = 20;
+
+    public const Types = [
+        "entity.taxe.label.type_percentage" => self::TYPE_PERCENTAGE,
+        "entity.taxe.label.type_amount" => self::TYPE_AMOUNT,
+    ];
+
+    const ReverseTypes = [
+        self::TYPE_AMOUNT => "entity.taxe.label.type_amount",
+        self::TYPE_PERCENTAGE => "entity.taxe.label.type_percentage",
+    ];
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
@@ -29,11 +42,6 @@ class Taxe
     private $user;
 
     /**
-     * @ORM\Column(type="integer")
-     */
-    private $priority;
-
-    /**
      * @ORM\Column(type="decimal", precision=50, scale=5)
      */
     private $value;
@@ -47,6 +55,21 @@ class Taxe
      * @ORM\ManyToMany(targetEntity="App\Entity\Recipe", mappedBy="taxes")
      */
     private $recipes;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $Type;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isDefault;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isEnabledForCommunity;
 
     public function __construct()
     {
@@ -70,26 +93,14 @@ class Taxe
         return $this;
     }
 
-    public function getUser(): ?User
+    public function getUser(): ?UserInterface
     {
         return $this->user;
     }
 
-    public function setUser(?User $user): self
+    public function setUser(?UserInterface $user): self
     {
         $this->user = $user;
-
-        return $this;
-    }
-
-    public function getPriority(): ?int
-    {
-        return $this->priority;
-    }
-
-    public function setPriority(int $priority): self
-    {
-        $this->priority = $priority;
 
         return $this;
     }
@@ -142,6 +153,47 @@ class Taxe
             $this->recipes->removeElement($recipe);
             $recipe->removeTax($this);
         }
+
+        return $this;
+    }
+
+    public function getTypeStr(): ?string
+    {
+        return self::ReverseTypes[$this->getType()];
+    }
+
+    public function getType(): ?int
+    {
+        return $this->Type;
+    }
+
+    public function setType(int $Type): self
+    {
+        $this->Type = $Type;
+
+        return $this;
+    }
+
+    public function getIsDefault(): ?bool
+    {
+        return $this->isDefault;
+    }
+
+    public function setIsDefault(bool $isDefault): self
+    {
+        $this->isDefault = $isDefault;
+
+        return $this;
+    }
+
+    public function getIsEnabledForCommunity(): ?bool
+    {
+        return $this->isEnabledForCommunity;
+    }
+
+    public function setIsEnabledForCommunity(bool $isEnabledForCommunity): self
+    {
+        $this->isEnabledForCommunity = $isEnabledForCommunity;
 
         return $this;
     }
---
+++ src/Form/TaxeType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Taxe;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaxeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('libelle', TextType::class, [
+                'label' => 'entity.taxe.label.libelle',
+                'required' => true,
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'entity.taxe.label.description',
+                'required' => false,
+
+            ])
+            ->add('value', NumberType::class, [
+                'scale' => 5,
+                'label' => 'entity.taxe.label.value',
+                'required' => true,
+            ])
+            ->add('isDefault', CheckboxType::class, [
+                'required' => false,
+                'label' => 'entity.taxe.label.isDefault',
+            ])
+            ->add('type', ChoiceType::class, [
+                'choices' => Taxe::Types,
+                'required' => true,
+                'label' => 'entity.taxe.label.type',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Taxe::class,
+        ]);
+    }
+}
