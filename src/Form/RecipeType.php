abb513685bbd4690bdc2d9855869946d963aacc3
Pierrick <p.pobelle@id2i.net>
2020-01-28 13:34:56 +0100

    debut des taxes

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -10,38 +10,54 @@ use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class RecipeType extends AbstractType
 {
+    /**
+     * @var \Symfony\Component\Security\Core\Authentication\Token\TokenInterface|null
+     */
+    private $token;
+
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+$this->token = $tokenStorage->getToken();
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $user = $this->token->getUser();
         $builder
-            ->add('label', null,[
-                'label'=>'entity.recipe.label.label',
-                'required'=>true,
+            ->add('label', null, [
+                'label' => 'entity.recipe.label.label',
+                'required' => true,
             ])
-            ->add('marge', null,[
-                'label'=>'entity.recipe.label.marge',
-                'required'=>true,
+            ->add('marge', null, [
+                'label' => 'entity.recipe.label.marge',
+                'required' => true,
             ])
-            ->add('estimatedHours', NumberType::class,[
-                'label'=>'entity.recipe.label.estimatedHours',
-                'required'=>true,
-                'scale'=>4,
+            ->add('estimatedHours', NumberType::class, [
+                'label' => 'entity.recipe.label.estimatedHours',
+                'required' => true,
+                'scale' => 4,
             ])
-//            ->add('taxes', EntityType::class, [
-//                'class' => Taxe::class,
-//                'multiple'=>true,
-//                'label'=>'entity.recipe.label.taxes',
-//
-//
-//            ])
+            ->add('taxes', EntityType::class, [
+                'class' => Taxe::class,
+                'multiple' => true,
+                'choice_label' => function (Taxe $taxe) use($user) {
+                    return $taxe->getLibelle() . " " . number_format($taxe->getValue(),2,',',' ')." ".
+                        ($taxe->getType() == Taxe::TYPE_PERCENTAGE ? "%" : $user->getMoneyunit());
+                },
+                'expanded' => true,
+                'label' => 'entity.recipe.label.taxes',
+
+            ])
             ->add('recipeComponents', CollectionType::class, [
                 'entry_type' => RecipeComponentType::class,
                 'allow_add' => true,
-                'label'=>'entity.recipe.label.recipeComponents',
+                'label' => 'entity.recipe.label.recipeComponents',
 
-                'allow_delete'=>true,
+                'allow_delete' => true,
                 'entry_options' => [
                     'attr' => ['class' => 'row component-box'],
                 ],
